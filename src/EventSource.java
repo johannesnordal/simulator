85f497576d9e43bd1896310b5b6f28ce81e6f2a1
Jóhannes Nordal <johannesnordal88@gmail.com>
2021-04-10 18:07:51 +0000

    Add: register observers in bulk

--- src/EventSource.java
+++ src/EventSource.java
@@ -1,8 +1,25 @@
 package spool;
 
+import java.lang.RuntimeException;
+
 public interface EventSource
 {
-    public Observer[] getObservers();
     public void registerObserver(Observer observer);
     public void registerEvent(Event event, Client client);
+
+    public static void registerObserver(EventSource[] eventSource,
+            Observer[] observer)
+    {
+        if (eventSource.length != observer.length)
+        {
+            String e = "Arrays of different length";
+
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < eventSource.length; i++)
+        {
+            eventSource[i].registerObserver(observer[i]);
+        }
+    }
 }
