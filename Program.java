367bceecf67dd2f075cda512ea75467db650c56e
Jóhannes Nordal <johannesnordal88@gmail.com>
2021-04-10 02:09:20 +0000

    ...

--- Program.java
+++ Program.java
@@ -1,18 +1,15 @@
-import java.util.HashSet;
+import java.lang.reflect.*;
+import java.util.*;
 import spool.*;
 
-public class Program {
-    public static void main(String[] args) {
-        Distribution arrival;
-        Distribution service;
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Client x = new Client(0.0, 1.0);
+        Client y = x.clone();
 
-        HashSet<String> opts = new HashSet<>();
-
-        opts.add("-s");
-        opts.add("-d");
-        opts.add("-x");
-        opts.add("-y");
-
-
+        Distribution arrival = new Exponential(1.0);
+        System.out.println(Exponential.class == arrival.getClass());
     }
 }
--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-for i in 2 4 8
-do
-    java -cp .:dist/spool.jar Program $i 15 > cv49-P-res-gt-"$i"ser-SRPT.txt
-    echo SRPT $i is done
-done
+javac -cp .:dist/spool.jar Program.java
+java -cp .:dist/spool.jar Program
